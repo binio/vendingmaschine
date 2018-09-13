81dd28716a6a85118335fc2de24639ec7dd3f51a
binio <tbiniecki@gmail.com>
2018-09-13 22:27:02 +0100

    creating objects and classes

--- src/main/scala/com/binio/tutorial/Main.scala
+++ src/main/scala/com/binio/tutorial/Main.scala
@@ -1,19 +1,24 @@
 package com.binio.tutorial
 
+//name will not be accesible
 class Dragon(name: String)
-class Dog(val name:String)
+
+//we can omit name in constructor then Unknown will be used
+class Dog(val name:String = "Unknown")
 
 object Main extends  App {
 
   val a = new Dragon("Thomas")
   val b = new Dragon("Rob")
-  val c = new Dog("Kuba")
-  val d = new Dog("Reksio")
+  val c = new Dog("Kuba")    //with accesible name
+  val d = new Dog("Reksio")  //with accesible name
+  val e = new Dog()                  //with Unknown name
 
 
   println(a)
   println(b)
   println(c.name)
   println(d.name)
+  println(e.name)
 
 }
