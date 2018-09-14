7538b28dc7188e0b0b9d303eb1bf1425328ea8d4
binio <tbiniecki@gmail.com>
2018-09-14 23:25:27 +0100

    companion object

--- src/main/scala/com/binio/tutorial/Main.scala
+++ src/main/scala/com/binio/tutorial/Main.scala
@@ -9,6 +9,28 @@ class Dog(val name:String = "Unknown")
 //named parameters
 class Coordinate (val lattitude:Double, val longitude:Double)
 
+//companion object
+
+object Dictionary {
+
+  val sentenceA = "Hi there!"
+  def sentenceB(n: String) = s"Welcome $n!"
+  val sentenceC = "What's up bro"
+}
+
+
+abstract class Robot(val name: String) {
+  def welcome(name: String):String
+}
+
+object Robot {
+  def mostTalkative(robot1: Robot, robot2: Robot):Robot = {
+    val r1size = robot1.welcome("Tom").length
+    val r2size = robot2.welcome("Bob").length
+    if(r1size > r2size) robot1 else robot2
+  }
+}
+
 object Main extends  App {
 
   val a = new Dragon("Thomas")
@@ -18,6 +40,13 @@ object Main extends  App {
   val e = new Dog()                  //with Unknown name
   val cord = new Coordinate(lattitude = 12.1, longitude = 10.9)
 
+  val tom = new Robot("Tom 1") { def welcome(n: String) = Dictionary.sentenceA } // we compose new object and its welcome function out of dictionary
+  val bob = new Robot("Bob 1") { def welcome(n: String) = Dictionary.sentenceB(n) }
+
+  println(Robot.mostTalkative(tom, bob).name)
+  println(tom.welcome("test"))
+  println(bob.welcome("test"))
+
 
   println(a)
   println(b)
@@ -26,5 +55,7 @@ object Main extends  App {
   println(e.name)
   println(cord.lattitude)
   println(cord.longitude)
+
+
 
 }
