986d2b68626c41470ce2d911b56e0402af8baae6
binio <tbiniecki@gmail.com>
2018-09-15 17:58:03 +0100

    trait extending class

--- src/main/scala/com/binio/traits/Main.scala
+++ src/main/scala/com/binio/traits/Main.scala
@@ -4,7 +4,11 @@ trait Cute {
   def fun():String = "I am cute city"
 }
 
+class House(val rooms:Int)
 class City(val name:String) extends Cute
+trait BigTrait extends House {
+  def fun():String = "I am big house"
+}
 
 object Main extends App{
 
@@ -12,5 +16,17 @@ object Main extends App{
   val city = new City("Wejherowo")
   println(city.name)
   println(city.fun())
+
+  //2. TRait can extend class
+  val house = new House(5)
+  println(house.rooms + " rooms")
+
+  //3. Class extends Big Trait
+  //class Mistake(override val rooms:Int) extends BigTrait
+  //val mistake = new Mistake(7)
+  //println(mistake.rooms)
+  //println(mistake.fun())
+  //does not compile Unspecified value parameter rooms.
+  //[error]   class Mistake(override val rooms:Int) extends Big
 
 }
