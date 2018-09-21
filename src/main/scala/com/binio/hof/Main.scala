4abd0eaac0ea3cfe39f77b94c91569eb5c058079
binio <tbiniecki@gmail.com>
2018-09-21 19:32:02 +0100

    HOF

--- src/main/scala/com/binio/hof/Main.scala
+++ src/main/scala/com/binio/hof/Main.scala
@@ -2,6 +2,36 @@ package com.binio.hof
 
 object Main extends App {
 
+  private def abs(n:Int):Int = {
+    if(n < 0) n
+    else n
+  }
+
+  private def formatAbs(x:Int):String = {
+    val msg = "The absolute value of %d id %d"
+    msg.format(x, abs(x))
+  }
+
+  private def formatFactorial(x: Int):String ={
+    val msg = "The factorial value of %d id %d"
+    msg.format(x, abs(x))
+  }
+
+  private def formatResult(name:String, n:Int, f: Int => Int):String = {
+    val msg = "The %s of %d id %d"
+    msg.format(name, n, f(n))
+  }
+
+  def factorial(n: Int):Int = {
+    def loop(n:Int, acc:Int): Int ={
+      if(n <=0 ) acc
+      else loop(n - 1, n*acc)
+    }
+    loop(n, 1)
+  }
+
+
+
   def fib(n:Int):Int = {
     def loop(n:Int): Int =
       if(n < 2) n
@@ -16,4 +46,14 @@ object Main extends App {
   println(fib(2))
   println(fib(3))
   println(fib(4))
+
+  println(formatAbs(abs(7)))
+  println(formatFactorial(factorial(10)))
+
+  //converting to HOF non polymorfic version
+  println(formatResult("factorial", 10, factorial))
+  println(formatResult("absolute value", 10, abs))
+
+
+
 }
