ed77f0d7b014dd2fc6f6384bfbef952b55d5a045
binio <tbiniecki@gmail.com>
2018-09-23 09:22:46 +0100

    HOF cd

--- src/main/scala/com/binio/hof/Main.scala
+++ src/main/scala/com/binio/hof/Main.scala
@@ -40,6 +40,50 @@ object Main extends App {
     loop(n)
   }
 
+  def findFirst(ss: Array[String], key: String): Int = {
+    def loop(n: Int): Int =
+      if(n >= ss.length) -1
+      else if (ss(n) == key) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  def findFirstx[A](ss: Array[A], f: A => Boolean): Int = {
+    def loop(n: Int): Int =
+      if(n >= ss.length) -1
+      else if (f(ss(n))) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  //this check if sorted from low to high only, strange
+  def isSorted[A](as: Array[A], ordering: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean =
+      if (n >= as.length - 1) true
+      else if (ordering(as(n), as(n + 1))) {
+        println("ordering " + ordering(as(n), as(n + 1)))
+
+        false //this false is important
+      }
+      else go(n + 1)
+
+    go(0)
+  }
+
+
+  //check if elements are the same
+  def checkTheSame[A](as:Array[A], same:(A,A) => Boolean): Boolean = {
+    def go(n:Int): Boolean =
+      if(n>= as.length -1)  true
+      else if(!same(as(n), as(n+1))) false
+      else go(n+1)
+
+    go(0)
+  }
+
 
   println(fib(0))
   println(fib(1))
@@ -53,6 +97,20 @@ object Main extends App {
   //converting to HOF non polymorfic version
   println(formatResult("factorial", 10, factorial))
   println(formatResult("absolute value", 10, abs))
+
+  //find non polymorfic
+  println(findFirst(Array("kkk", "hello", "ooops"), "hello"))
+  println(findFirstx(Array("kkk", "hello", "ooops", "tomasz"), (x:String) => x =="hello"))
+
+
+  //usage of isSorted function
+  println(isSorted(Array(1, 3, 5, 7, 8), (x: Int, y: Int) => x > y))
+  println(isSorted(Array(7, 5, 3, 1), (x: Int, y: Int) => x < y))
+  println(isSorted(Array("Scala", "Exercises"), (x: String, y: String) => x.length > y.length))
+
+  //check if elements are the same
+  println("The same:" + checkTheSame(Array(7, 5, 3, 1), (x: Int, y: Int) => x == y))
+  println("The same:" + checkTheSame(Array(1, 1, 1, 1), (x: Int, y: Int) => x == y))
 
 
 
