2f82e584f7204eddb880f1d3b9f9c9e7519badfe
binio <tbiniecki@gmail.com>
2018-09-30 17:53:49 +0100

    add drop and dropWhile

--- src/main/scala/patmatch/Main.scala
+++ src/main/scala/patmatch/Main.scala
@@ -38,7 +38,7 @@ object Main extends App{
     }
 
     def dropWhile[A](l:List[A], f:A => Boolean):List[A] =  l match {
-      case Cons(h, t) if(f) => dropWhile(t,f)
+      case Cons(h, t) if(f(h)) => dropWhile(t,f)
       case _ => l
 
     }
